4801b601bb8a377627cd25be886c3fc6252e57b0
Juliano Santos <julianosantosdev@outlook.com>
2024-12-27 12:36:38 -0300

    promises

--- 03-assynchronous-JS/index.js
+++ 03-assynchronous-JS/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const superagent = require('superagent');
 
 /* ------------------------------ CALLBACK HELL ----------------------------- */
-fs.readFile(`${__dirname}/dog.txt`, (error, data) => {
+/* fs.readFile(`${__dirname}/dog.txt`, (error, data) => {
   console.log(`Breed: ${data}`);
 
   superagent
@@ -14,10 +14,10 @@ fs.readFile(`${__dirname}/dog.txt`, (error, data) => {
         console.log('Random dog image saved to file!');
       });
     });
-});
+}); */
 
 /* ------------------------------ USING 'THEN' ------------------------------ */
-fs.readFile(`${__dirname}/dog.txt`, (error, data) => {
+/* fs.readFile(`${__dirname}/dog.txt`, (error, data) => {
   console.log(`Breed: ${data}`);
 
   superagent
@@ -31,4 +31,39 @@ fs.readFile(`${__dirname}/dog.txt`, (error, data) => {
     .catch((error) => {
       console.log(error.message);
     });
-});
+}); */
+
+/* ------------------------ USING PROMISES (BEST WAY) ----------------------- */
+const readFilePromise = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (error, data) => {
+      if (error) reject('I could not find that file...😢');
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePromise = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (error) => {
+      if (error) reject('Could not write file 😢');
+      resolve('success!');
+    });
+  });
+};
+
+readFilePromise(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((response) => {
+    console.log(response.body.message);
+    return writeFilePromise('dog-img.txt', response.body.message);
+  })
+  .then(() => {
+    console.log('Random dog image saved to file!');
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
