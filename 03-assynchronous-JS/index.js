8bc82db8288652ebcf40453d5685c6b199988d6d
Juliano Santos <julianosantosdev@outlook.com>
2024-12-27 10:46:54 -0300

    starting solve callback hell

--- 03-assynchronous-JS/index.js
+++ 03-assynchronous-JS/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
+/* ------------------------------ CALLBACK HELL ----------------------------- */
 fs.readFile(`${__dirname}/dog.txt`, (error, data) => {
   console.log(`Breed: ${data}`);
 
@@ -12,5 +13,22 @@ fs.readFile(`${__dirname}/dog.txt`, (error, data) => {
       fs.writeFile('dog-img.txt', response.body.message, (error) => {
         console.log('Random dog image saved to file!');
       });
+    });
+});
+
+/* ------------------------------ USING 'THEN' ------------------------------ */
+fs.readFile(`${__dirname}/dog.txt`, (error, data) => {
+  console.log(`Breed: ${data}`);
+
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then((response) => {
+      console.log(response.body.message);
+      fs.writeFile('dog-img.txt', response.body.message, (error) => {
+        console.log('Random dog image saved to file!');
+      });
+    })
+    .catch((error) => {
+      console.log(error.message);
     });
 });
