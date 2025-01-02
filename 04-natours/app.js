2389d8933b9fb4bbecfc48746247b8e634a2cf7b
Juliano Santos <julianosantosdev@outlook.com>
2025-01-02 18:40:00 -0300

    express routes and request

--- 04-natours/app.js
+++ 04-natours/app.js
@@ -1,11 +1,25 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 
-app.get('/', (request, response) => {
-  response
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
+// app.get('/', (request, response) => {
+//   response
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`)
+);
+
+app.get('/api/v1/tours', (request, response) => {
+  response.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
