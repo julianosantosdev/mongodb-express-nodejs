492d9e0c38cfe043b3be69aea29cb9abc7df701f
Juliano Santos <julianosantosdev@outlook.com>
2025-01-24 11:51:11 -0300

    mongoose setup

--- 04-natours/server.js
+++ 04-natours/server.js
@@ -1,6 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DBAtlas = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+// const DBLocal = process.env.DATABASE_LOCAL;
+
+mongoose
+  .connect(DBAtlas, {
+    // useNewUrlParser: true, not used anymore
+    // useCreateIndex: true, not used anymore
+    // useFindAndModify: false, not used anymore
+  })
+  .then((connect) => {
+    console.log('DB Connection Successfull');
+  });
 
 const app = require('./app');
 
