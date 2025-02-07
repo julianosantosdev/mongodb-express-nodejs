cc130346b2e28c6859b18480a2684eb5edadd193
Juliano Santos <julianosantosdev@outlook.com>
2025-02-07 17:07:22 -0300

    feat: import development data to db script

---
+++ 04-natours/dev-data/data/import-dev-data.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const fs = require('fs');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DBAtlas = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DBAtlas).then(() => {
+  console.log('DB Connection Successfull');
+});
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM COLLECTION
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') importData();
+if (process.argv[2] === '--delete') deleteData();
+
+console.log(process.argv);
